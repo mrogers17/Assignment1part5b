166297d9a969fa8393cb0f74734eeb2ee57fabff
Jacob Lawrence Morrow <jmorrow@cse.unl.edu>
2017-09-06 22:57:24 -0500

    Switched name order

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
  class Refraction {
    public static void main(String[] args) {
 
-   System.out.print("Mary Clare Rogers and Jacob Morrow")
+   System.out.print("Jacob Morrow and Mary Clare Rogers")
 
  }
 }
